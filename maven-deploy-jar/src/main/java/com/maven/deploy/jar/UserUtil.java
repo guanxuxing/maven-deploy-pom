93fc619e5bd578e3a7ebb4321611cdf67600e20a
gxx <844155360@qq.com>
2018-09-13 21:30:39 +0800

    base

---
+++ maven-deploy-jar/src/main/java/com/maven/deploy/jar/UserUtil.java
@@ -0,0 +1,11 @@
+package com.maven.deploy.jar;
+
+/**
+ * Created by Administrator on 2018/7/22.
+ */
+public class UserUtil {
+
+    public static void show (){
+        System.out.println("deploy.show.test.123.456.789");
+    }
+}
